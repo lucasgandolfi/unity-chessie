c6711f3f609e77a44ca3318cee0891163074f8e9
lucasgandolfi <lucas_gandolfi@terra.com.br>
2019-05-12 03:23:13 -0300

    BoardNodes and PieceMoves
    basic start

---
+++ zerone/Assets/Scripts/BoardNodes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardNodes : MonoBehaviour
+{
+    public static Transform[] nodes;
+    private void Awake() {
+        nodes = new Transform[transform.childCount];
+        for( int i = 0; i < nodes.Length; i++)
+        {
+            nodes[i] = transform.GetChild(i);
+
+        }
+    }
+
+}
---
+++ zerone/Assets/Scripts/PieceMoves.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMoves : MonoBehaviour
+{
+    public float movSpeed = 10f;
+    private Transform target;
+    private int nodesIndex = 0;
+
+    private void Start() {
+        
+        target = BoardNodes.nodes[0];
+    }
+    private void Update() {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * movSpeed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextNode();
+        }
+ 
+    }
+
+    void GetNextNode()
+    {
+        if (nodesIndex >= BoardNodes.nodes.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        nodesIndex ++;
+        target = BoardNodes.nodes[nodesIndex];
+    }
+
+
+}
